338914933eacdd6a78f00df32d0024caa35fb651
Marc-Marquez-93 <miguel.duran.14.17@outlook.com>
2025-11-26 22:03:45 -0500

    terminacion de inicio

--- src/views/inicio.vue
+++ src/views/inicio.vue
@@ -1,11 +1,13 @@
-<script setup>
-import inicioJS from "../composables/categoria.js"
-
-</script>
-
 <template>
     <div class="container">
         <div class="init">
+            <q-chip color="orange" text-color="white" icon="star" style="font-size: 1.5em; user-select: none;">
+                El ahorcado
+            </q-chip>
+            <div class="h2r">
+                <h2>Bienvenido!!!, no dejes que el tiempo te gane y adivina la palabra. Dale al boton 👀</h2>
+            </div>
+            
             <q-btn color="purple" label="categoria" to="/categoria" /><br>
         </div>
     </div>
