1e1e0e1117c9d6f8d4d48f187a13d12a25d918e1
Marc-Marquez-93 <miguel.duran.14.17@outlook.com>
2025-11-26 21:44:44 -0500

    inicio 1.0

--- src/composables/juego.js
+++ src/composables/juego.js
@@ -4,29 +4,30 @@ const json = ref([]);
 
 (async () => {
 
-    try {
-        const data = await import("../data/palabras.json");
-        json.value = data.default ?? data;
-        console.log("JSON precargado:", json.value);
-    }
-    catch (error) {
-        console.log("se presento un error cargango el JSON")
-    }
+  try {
+    const data = await import("../data/palabras.json");
+    json.value = data.default ?? data;
+    console.log("JSON precargado:", json.value);
+  } 
+  catch (error) {
+    console.error("❌ Error cargando el JSON:", error);
+  }
 
 })();
 
 const juego = {
-    useAhorcado: async () => {
+  useAhorcado: async () => {
 
-    },
+  },
 
-    useFrutas: async () => {
+  useFrutas: async () => {
 
-    },
+  },
 
-    useConfig: async () => {
+  useConfig: async () => {
 
-    },
+  },
 };
 
+export { json };
 export default juego;
--- src/views/inicio.vue
+++ src/views/inicio.vue
@@ -4,10 +4,10 @@ import inicioJS from "../composables/categoria.js"
 </script>
 
 <template>
-    <div>
-        <h1>hola, soy inicio</h1>
-
-        <q-btn color="primary" label="categoria" to="/categoria" /><br>
+    <div class="container">
+        <div class="init">
+            <q-btn color="purple" label="categoria" to="/categoria" /><br>
+        </div>
     </div>
 </template>
 
