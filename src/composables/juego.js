d608a5e7798ba932ed0a4175771d09082e821d2d
Marc-Marquez-93 <miguel.duran.14.17@outlook.com>
2025-11-26 20:15:54 -0500

    prueba de carga del json

--- src/composables/juego.js
+++ src/composables/juego.js
@@ -1,5 +1,13 @@
 import { ref } from "vue";
 
+const json = ref([]);
+
+(async () => {
+  const data = await import("../data/palabras.json");
+  json.value = data.default ?? data;
+  console.log("JSON precargado:", json.value);
+})();
+
 const juego = {
   useAhorcado: async () => {
     
